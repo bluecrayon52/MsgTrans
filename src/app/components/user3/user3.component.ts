303fa7051122a681201ff95d361b4da161fe0ad1
bluecrayon52 <16687465+bluecrayon52@users.noreply.github.com>
2017-11-13 22:10:44 -0500

    debug weighted speed

--- src/app/components/user3/user3.component.ts
+++ src/app/components/user3/user3.component.ts
@@ -9,31 +9,19 @@ export class User3Component implements OnInit {
   canvas: HTMLCanvasElement;
   ctx:  CanvasRenderingContext2D;
 
-  // edge weights for the red ball
-  link1 = 0;
-  link2 = 0;
-  link3 = 0;
-  link4 = 0;
-  link5 = 0;
-  link6 = 0;
-  link7 = 0;
-  link8 = 0;
-  link9 = 0;
-  link10 = 0;
-  link11 = 0;
+  links = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  // edge weights for the red b all
 
+  linksB = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   // edge weights for the blue ball
-  link1B = 0;
-  link2B = 0;
-  link3B = 0;
-  link4B = 0;
-  link5B = 0;
-  link6B = 0;
-  link7B = 0;
-  link8B = 0;
-  link9B = 0;
-  link10B = 0;
-  link11B = 0;
+
+  // normalized edge weights where 3, 4, 5, 6, 7 become 3, 5, 8, 10, 13
+  // Red
+  weights = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  // Blue
+  weightsB = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
 
   graph: any;
   graphB: any;  // blue
@@ -56,6 +44,7 @@ export class User3Component implements OnInit {
 
     // init graph edge weights
     this.graphSet();
+    this.setWeights();
 
     // set up the canvas
     this.canvas = <HTMLCanvasElement> document.getElementById('graph');
@@ -81,32 +70,13 @@ export class User3Component implements OnInit {
   }
 
   graphSet() {
-    // init edge weights for red ball
-    this.link1 = 1 + Math.floor(Math.random() * 7);
-    this.link2 = 1 + Math.floor(Math.random() * 7);
-    this.link3 = 1 + Math.floor(Math.random() * 7);
-    this.link4 = 1 + Math.floor(Math.random() * 7);
-    this.link5 = 1 + Math.floor(Math.random() * 7);
-    this.link6 = 1 + Math.floor(Math.random() * 7);
-    this.link7 = 1 + Math.floor(Math.random() * 7);
-    this.link8 = 1 + Math.floor(Math.random() * 7);
-    this.link9 = 1 + Math.floor(Math.random() * 7);
-    this.link10 = 1 + Math.floor(Math.random() * 7);
-    this.link11 = 1 + Math.floor(Math.random() * 7);
 
-    // init edge weights for blue ball
-    this.link1B = 1 + Math.floor(Math.random() * 7);
-    this.link2B = 1 + Math.floor(Math.random() * 7);
-    this.link3B = 1 + Math.floor(Math.random() * 7);
-    this.link4B = 1 + Math.floor(Math.random() * 7);
-    this.link5B = 1 + Math.floor(Math.random() * 7);
-    this.link6B = 1 + Math.floor(Math.random() * 7);
-    this.link7B = 1 + Math.floor(Math.random() * 7);
-    this.link8B = 1 + Math.floor(Math.random() * 7);
-    this.link9B = 1 + Math.floor(Math.random() * 7);
-    this.link10B = 1 + Math.floor(Math.random() * 7);
-    this.link11B = 1 + Math.floor(Math.random() * 7);
-
+    for ( let i = 0; i < 11; i++) {
+      this.links[i] = 1 + Math.floor(Math.random() * 7);
+      this.linksB[i] = 1 + Math.floor(Math.random() * 7);
+      // this.links[i] = 7;
+      // this.linksB[i] = 7;
+    }
 
     // Test complex weight pattern
     // this.link1 = 1;
@@ -122,27 +92,58 @@ export class User3Component implements OnInit {
     // this.link11 = 1;
 
     this.graph = {
-      start: {A: this.link1, B: this.link2},
-      A: {C: this.link3, E: this.link5},
-      B: {C: this.link4, F: this.link7},                // bidirectional from (A, C), (C, A) & (B, C), (C, B)
-      C: {A: this.link3, B: this.link4, D: this.link6}, // bidirectional from (C, D), (D, C)
-      D: {C: this.link6, E: this.link8, F: this.link9}, // bidirectional from (E, D), (D, E) & (F, D), (D, F)
-      E: {D: this.link8, finish: this.link10},
-      F: {D: this.link9, finish: this.link11},
+      start: {A: this.links[0], B: this.links[1]},
+      A: {C: this.links[2], E: this.links[4]},
+      B: {C: this.links[3], F: this.links[6]},                // bidirectional from (A, C), (C, A) & (B, C), (C, B)
+      C: {A: this.links[2], B: this.links[3], D: this.links[5]}, // bidirectional from (C, D), (D, C)
+      D: {C: this.links[5], E: this.links[7], F: this.links[8]}, // bidirectional from (E, D), (D, E) & (F, D), (D, F)
+      E: {D: this.links[7], finish: this.links[9]},
+      F: {D: this.links[8], finish: this.links[10]},
       finish: {}
     };
 
     this.graphB = {
-      start: {A: this.link1B, B: this.link2B},
-      A: {C: this.link3B, E: this.link5B},
-      B: {C: this.link4B, F: this.link7B},                // bidirectional from (A, C), (C, A) & (B, C), (C, B)
-      C: {A: this.link3B, B: this.link4B, D: this.link6B}, // bidirectional from (C, D), (D, C)
-      D: {C: this.link6B, E: this.link8B, F: this.link9B}, // bidirectional from (E, D), (D, E) & (F, D), (D, F)
-      E: {D: this.link8B, finish: this.link10B},
-      F: {D: this.link9B, finish: this.link11B},
+      start: {A: this.linksB[0], B: this.linksB[1]},
+      A: {C: this.linksB[2], E: this.linksB[4]},
+      B: {C: this.linksB[3], F: this.linksB[6]},                // bidirectional from (A, C), (C, A) & (B, C), (C, B)
+      C: {A: this.linksB[2], B: this.linksB[3], D: this.linksB[5]}, // bidirectional from (C, D), (D, C)
+      D: {C: this.linksB[5], E: this.linksB[7], F: this.linksB[8]}, // bidirectional from (E, D), (D, E) & (F, D), (D, F)
+      E: {D: this.linksB[7], finish: this.linksB[9]},
+      F: {D: this.linksB[8], finish: this.linksB[10]},
       finish: {}
     };
 
+  }
+
+  setWeights() {
+    for (let i = 0; i < 11; i++) {
+      switch (this.links[i]) {
+        case 3: this.weights[i] = 5 / 4;
+        break;
+        case 4: this.weights[i] =  1;
+        break;
+        case 5: this.weights[i] = 5 / 8;
+        break;
+        case 6: this.weights[i] = 1 / 2;
+        break;
+        case 7: this.weights[i] = 5 / 16;
+        break;
+        default: this.weights[i] = 5 / this.links[i];
+      }
+      switch (this.linksB[i]) {
+        case 3: this.weightsB[i] = 5 / 4;
+        break;
+        case 4: this.weightsB[i] =  1;
+        break;
+        case 5: this.weightsB[i] = 5 / 8;
+        break;
+        case 6: this.weightsB[i] = 1 / 2;
+        break;
+        case 7: this.weightsB[i] = 5 / 16;
+        break;
+        default: this.weightsB[i] = 5 / this.linksB[i];
+      }
+    }
   }
 
 // source reference: https://gist.github.com/jpillora/7382441
@@ -262,21 +263,21 @@ dijkstra(graph, s) {
     // display edge weights
     ctx.font = '30px Arial ';
     ctx.fillStyle = 'red';
-    ctx.fillText(this.link1.toString(), 310, 300);
-    ctx.fillText(this.link2.toString(), 310, 600);
+    ctx.fillText(this.links[0].toString(), 310, 300);
+    ctx.fillText(this.links[1].toString(), 310, 600);
 
-    ctx.fillText(this.link3.toString(), 535, 300);
-    ctx.fillText(this.link4.toString(), 535, 600);
+    ctx.fillText(this.links[2].toString(), 535, 300);
+    ctx.fillText(this.links[3].toString(), 535, 600);
 
-    ctx.fillText(this.link5.toString(), 785, 175);
-    ctx.fillText(this.link6.toString(), 785, 425);
-    ctx.fillText(this.link7.toString(), 785, 675);
+    ctx.fillText(this.links[4].toString(), 785, 175);
+    ctx.fillText(this.links[5].toString(), 785, 425);
+    ctx.fillText(this.links[6].toString(), 785, 675);
 
-    ctx.fillText(this.link8.toString(), 1060, 300);
-    ctx.fillText(this.link9.toString(), 1060, 600);
+    ctx.fillText(this.links[7].toString(), 1060, 300);
+    ctx.fillText(this.links[8].toString(), 1060, 600);
 
-    ctx.fillText(this.link10.toString(), 1285, 300);
-    ctx.fillText(this.link11.toString(), 1285, 600);
+    ctx.fillText(this.links[9].toString(), 1285, 300);
+    ctx.fillText(this.links[10].toString(), 1285, 600);
 
     ctx.fillStyle = 'black';
     ctx.fillText('(   /   )', 300, 300);
@@ -296,21 +297,21 @@ dijkstra(graph, s) {
     ctx.fillText('(   /   )', 1275, 600);
 
     ctx.fillStyle = 'blue';
-    ctx.fillText(this.link1B.toString(), 350, 300);
-    ctx.fillText(this.link2B.toString(), 350, 600);
+    ctx.fillText(this.linksB[0].toString(), 350, 300);
+    ctx.fillText(this.linksB[1].toString(), 350, 600);
 
-    ctx.fillText(this.link3B.toString(), 575, 300);
-    ctx.fillText(this.link4B.toString(), 575, 600);
+    ctx.fillText(this.linksB[2].toString(), 575, 300);
+    ctx.fillText(this.linksB[3].toString(), 575, 600);
 
-    ctx.fillText(this.link5B.toString(), 825, 175);
-    ctx.fillText(this.link6B.toString(), 825, 425);
-    ctx.fillText(this.link7B.toString(), 825, 675);
+    ctx.fillText(this.linksB[4].toString(), 825, 175);
+    ctx.fillText(this.linksB[5].toString(), 825, 425);
+    ctx.fillText(this.linksB[6].toString(), 825, 675);
 
-    ctx.fillText(this.link8B.toString(), 1100, 300);
-    ctx.fillText(this.link9B.toString(), 1100, 600);
+    ctx.fillText(this.linksB[7].toString(), 1100, 300);
+    ctx.fillText(this.linksB[8].toString(), 1100, 600);
 
-    ctx.fillText(this.link10B.toString(), 1325, 300);
-    ctx.fillText(this.link11B.toString(), 1325, 600);
+    ctx.fillText(this.linksB[9].toString(), 1325, 300);
+    ctx.fillText(this.linksB[10].toString(), 1325, 600);
 
   }
 
@@ -326,80 +327,80 @@ dijkstra(graph, s) {
 
       if (next === 'A') {
         if (prev === 'start') {
-          this.xPath.push(1);
-          this.yPath.push(-25 / 15);
+          this.xPath.push(this.weights[0]);
+          this.yPath.push((-5 / 3)  * this.weights[0]);
         } else if (prev === 'C') {
-          this.xPath.push(-1);
-          this.yPath.push(-25 / 15);
+          this.xPath.push(- this.weights[2]);
+          this.yPath.push((-5 / 3) * this.weights[2]);
           }
 
       } else if (next === 'B') {
         if (prev === 'start') {
-          this.xPath.push(1);
-          this.yPath.push(25 / 15);
+          this.xPath.push(this.weights[1]);
+          this.yPath.push((5 / 3) * this.weights[1]);
         } else if (prev === 'C') {
-          this.xPath.push(-1);
-          this.yPath.push(25 / 15);
+          this.xPath.push(- this.weights[3]);
+          this.yPath.push((5 / 3) * this.weights[3]);
           }
 
       } else if (next === 'C') {
         if (prev === 'A') {
-          this.xPath.push(1);
-          this.yPath.push(25 / 15);
+          this.xPath.push(this.weights[2]);
+          this.yPath.push((5 / 3) * this.weights[2]);
         } else if (prev === 'B') {
-          this.xPath.push(1);
-          this.yPath.push(-25 / 15);
+          this.xPath.push(this.weights[3]);
+          this.yPath.push((-5 / 3) * this.weights[3]);
 
         } else if (prev === 'D') {
-          this.xPath.push(-1);
+          this.xPath.push(- this.weights[5]);
           this.yPath.push(0);
           }
 
       } else if (next === 'D') {
         if (prev === 'C') {
-          this.xPath.push(1);
+          this.xPath.push(this.weights[5]);
           this.yPath.push(0);
         } else if (prev === 'E') {
-          this.xPath.push(-1);
-          this.yPath.push(25 / 15);
+          this.xPath.push(- this.weights[7]);
+          this.yPath.push((5 / 3) * this.weights[7]);
         } else if (prev === 'F') {
-          this.xPath.push(-1);
-          this.yPath.push(-25 / 15);
+          this.xPath.push(- this.weights[8]);
+          this.yPath.push((-5 / 3) * this.weights[8]);
           }
 
       } else if (next === 'E') {
         if (prev === 'A') {
-          this.xPath.push(1);
+          this.xPath.push(this.weights[4]);
           this.yPath.push(0);
-          this.xPath.push(1); // padding
+          this.xPath.push(this.weights[4]); // padding
           this.yPath.push(0);
-          this.xPath.push(1);
+          this.xPath.push(this.weights[4]);
           this.yPath.push(0);
         } else if (prev === 'D') {
-          this.xPath.push(1);
-          this.yPath.push(-25 / 15);
+          this.xPath.push(this.weights[7]);
+          this.yPath.push((-5 / 3) * this.weights[7]);
           }
 
       } else if (next === 'F') {
         if (prev === 'B') {
-          this.xPath.push(1);
+          this.xPath.push(this.weights[6]);
           this.yPath.push(0);
-          this.xPath.push(1); // padding
+          this.xPath.push(this.weights[6]); // padding
           this.yPath.push(0);
-          this.xPath.push(1);
+          this.xPath.push(this.weights[6]);
           this.yPath.push(0);
         } else if (prev === 'D') {
-          this.xPath.push(1);
-          this.yPath.push(25 / 15);
+          this.xPath.push(this.weights[8]);
+          this.yPath.push((5 / 3) * this.weights[8]);
           }
 
       } else if (next === 'finish') {
         if (prev === 'E') {
-          this.xPath.push(1);
-          this.yPath.push(25 / 15);
+          this.xPath.push(this.weights[9]);
+          this.yPath.push((5 / 3) * this.weights[9]);
         } else if (prev === 'F') {
-          this.xPath.push(1);
-          this.yPath.push(-25 / 15);
+          this.xPath.push(this.weights[10]);
+          this.yPath.push((-5 / 3) * this.weights[10]);
         }
 
       }
@@ -407,107 +408,101 @@ dijkstra(graph, s) {
     }
     this.xPath.reverse();
     this.yPath.reverse();
-    console.log('path: ' + this.path);
-    console.log('xPath: ' + this.xPath);
-    console.log('yPath: ' + this.yPath);
   }
 
   setSeqB() {
-    // reset the path arrays
-    this.xPathB = [];
-    this.yPathB = [];
-
-    // init prev
-    let prev = 'start';
-
-    for (const next of this.pathB) {
-
-      if (next === 'A') {
-        if (prev === 'start') {
-          this.xPathB.push(1);
-          this.yPathB.push(-25 / 15);
-        } else if (prev === 'C') {
-          this.xPathB.push(-1);
-          this.yPathB.push(-25 / 15);
-          }
-
-      } else if (next === 'B') {
-        if (prev === 'start') {
-          this.xPathB.push(1);
-          this.yPathB.push(25 / 15);
-        } else if (prev === 'C') {
-          this.xPathB.push(-1);
-          this.yPathB.push(25 / 15);
-          }
-
-      } else if (next === 'C') {
-        if (prev === 'A') {
-          this.xPathB.push(1);
-          this.yPathB.push(25 / 15);
-        } else if (prev === 'B') {
-          this.xPathB.push(1);
-          this.yPathB.push(-25 / 15);
-
-        } else if (prev === 'D') {
-          this.xPathB.push(-1);
-          this.yPathB.push(0);
-          }
-
-      } else if (next === 'D') {
-        if (prev === 'C') {
-          this.xPathB.push(1);
-          this.yPathB.push(0);
-        } else if (prev === 'E') {
-          this.xPathB.push(-1);
-          this.yPathB.push(25 / 15);
-        } else if (prev === 'F') {
-          this.xPathB.push(-1);
-          this.yPathB.push(-25 / 15);
-          }
-
-      } else if (next === 'E') {
-        if (prev === 'A') {
-          this.xPathB.push(1);
-          this.yPathB.push(0);
-          this.xPathB.push(1); // padding
-          this.yPathB.push(0);
-          this.xPathB.push(1);
-          this.yPathB.push(0);
-        } else if (prev === 'D') {
-          this.xPathB.push(1);
-          this.yPathB.push(-25 / 15);
-          }
-
-      } else if (next === 'F') {
-        if (prev === 'B') {
-          this.xPathB.push(1);
-          this.yPathB.push(0);
-          this.xPathB.push(1); // padding
-          this.yPathB.push(0);
-          this.xPathB.push(1);
-          this.yPathB.push(0);
-        } else if (prev === 'D') {
-          this.xPathB.push(1);
-          this.yPathB.push(25 / 15);
-          }
-
-      } else if (next === 'finish') {
-        if (prev === 'E') {
-          this.xPathB.push(1);
-          this.yPathB.push(25 / 15);
-        } else if (prev === 'F') {
-          this.xPathB.push(1);
-          this.yPathB.push(-25 / 15);
-        }
-
-      }
-      prev = next;
-    }
-    this.xPathB.reverse();
-    this.yPathB.reverse();
-    console.log('path: ' + this.pathB);
-    console.log('xPath: ' + this.xPathB);
-    console.log('yPath: ' + this.yPathB);
+     // reset the path arrays
+     this.xPathB = [];
+     this.yPathB = [];
+ 
+     // init prev
+     let prev = 'start';
+ 
+     for (const next of this.pathB) {
+ 
+       if (next === 'A') {
+         if (prev === 'start') {
+           this.xPathB.push(this.weightsB[0]);
+           this.yPathB.push((-5 / 3)  * this.weightsB[0]);
+         } else if (prev === 'C') {
+           this.xPathB.push(- this.weightsB[2]);
+           this.yPathB.push((-5 / 3) * this.weightsB[2]);
+           }
+ 
+       } else if (next === 'B') {
+         if (prev === 'start') {
+           this.xPathB.push(this.weightsB[1]);
+           this.yPathB.push((5 / 3) * this.weightsB[1]);
+         } else if (prev === 'C') {
+           this.xPathB.push(- this.weightsB[3]);
+           this.yPathB.push((5 / 3) * this.weightsB[3]);
+           }
+ 
+       } else if (next === 'C') {
+         if (prev === 'A') {
+           this.xPathB.push(this.weightsB[2]);
+           this.yPathB.push((5 / 3) * this.weightsB[2]);
+         } else if (prev === 'B') {
+           this.xPathB.push(this.weightsB[3]);
+           this.yPathB.push((-5 / 3) * this.weightsB[3]);
+ 
+         } else if (prev === 'D') {
+           this.xPathB.push(- this.weightsB[5]);
+           this.yPathB.push(0);
+           }
+ 
+       } else if (next === 'D') {
+         if (prev === 'C') {
+           this.xPathB.push(this.weightsB[5]);
+           this.yPathB.push(0);
+         } else if (prev === 'E') {
+           this.xPathB.push(- this.weightsB[7]);
+           this.yPathB.push((5 / 3) * this.weightsB[7]);
+         } else if (prev === 'F') {
+           this.xPathB.push(- this.weightsB[8]);
+           this.yPathB.push((-5 / 3) * this.weightsB[8]);
+           }
+ 
+       } else if (next === 'E') {
+         if (prev === 'A') {
+           this.xPathB.push(this.weightsB[4]);
+           this.yPathB.push(0);
+           this.xPathB.push(this.weightsB[4]); // padding
+           this.yPathB.push(0);
+           this.xPathB.push(this.weightsB[4]);
+           this.yPathB.push(0);
+         } else if (prev === 'D') {
+           this.xPathB.push(this.weightsB[7]);
+           this.yPathB.push((-5 / 3) * this.weightsB[7]);
+           }
+ 
+       } else if (next === 'F') {
+         if (prev === 'B') {
+           this.xPathB.push(this.weightsB[6]);
+           this.yPathB.push(0);
+           this.xPathB.push(this.weightsB[6]); // padding
+           this.yPathB.push(0);
+           this.xPathB.push(this.weightsB[6]);
+           this.yPathB.push(0);
+         } else if (prev === 'D') {
+           this.xPathB.push(this.weightsB[8]);
+           this.yPathB.push((5 / 3) * this.weightsB[8]);
+           }
+ 
+       } else if (next === 'finish') {
+         if (prev === 'E') {
+           this.xPathB.push(this.weightsB[9]);
+           this.yPathB.push((5 / 3) * this.weightsB[9]);
+         } else if (prev === 'F') {
+           this.xPathB.push(this.weightsB[10]);
+           this.yPathB.push((-5 / 3) * this.weightsB[10]);
+         }
+ 
+       }
+       prev = next;
+     }
+     this.xPathB.reverse();
+     this.yPathB.reverse();
   }
 
   animateBalls() {
@@ -599,56 +594,6 @@ dijkstra(graph, s) {
    }
    animateMe();
   }
-
-  animateBlue() {
-    const that = this;
-    let x = 150;
-    let y = 450;
-    let dx = 1;
-    let dy = 0;
-
-    function animateMe() {
-     requestAnimationFrame(animateMe); // infinate loop
-     that.ctx.clearRect(0, 0, innerWidth, innerHeight); // clear the canvas
-     that.draw();  // draw the default graph
-     that.blueCircle(x, y); // draw the circle
-
-     if (x === 300) {
-      dx = that.xPathB.pop();
-      dy = that.yPathB.pop();
-
-    } else if (x === 450) {
-      dx = that.xPathB.pop();
-      dy = that.yPathB.pop();
-
-    } else if (x === 600) {
-      dx = that.xPathB.pop();
-      dy = that.yPathB.pop();
-
-    } else if (x === 1050) {
-      dx = that.xPathB.pop();
-      dy = that.yPathB.pop();
-
-    } else if (x === 1200) {
-      dx = that.xPathB.pop();
-      dy = that.yPathB.pop();
-
-    } else if (x === 1350) {
-      dx = 1;
-      dy = 0;
-
-    } else if (x === 1500) {
-      dx = 0;
-      dy = 0;
-    }
-
-    x += dx;
-    y += dy;
-
-     console.log('Animate Blue');
-    }
-    animateMe();
-   }
 
   // takes in x and y coordinates and draws a red filled circle
   redCircle(xPos, yPos) {
